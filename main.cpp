ebfe83007a362ff081e2a3a1081a5fdb3c4c3617
adams <adamstano5@gmail.com>
2024-01-02 16:31:34 +0100

    server init

--- main.cpp
+++ main.cpp
@@ -1,6 +1,70 @@
 #include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <unistd.h>
+#include <winsock2.h>
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    // Inicializácia servera
+
+    // Vytvorenie socketu
+    WSADATA wsData;
+    if (WSAStartup(MAKEWORD(2, 2), &wsData) != 0) {
+        std::cerr << "Chyba pri inicializácii Winsock\n";
+        return -1;
+    }
+
+    // Vytvorenie socketu
+    SOCKET serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == INVALID_SOCKET) {
+        std::cerr << "Chyba pri vytváraní socketu\n";
+        WSACleanup();
+        return -1;
+    }
+
+    // Nastavenie adresy servera
+    sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(12345);  // Zvolte vhodný port
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+
+    // Pripojenie socketu k adrese
+    if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR) {
+        std::cerr << "Chyba pri viazaní socketu na adresu\n";
+        closesocket(serverSocket);
+        WSACleanup();
+        return -1;
+    }
+
+    // Čakanie na pripojenie hráčov
+    if (listen(serverSocket, 5) == SOCKET_ERROR) {
+        std::cerr << "Chyba pri čakaní na pripojenie hráčov\n";
+        closesocket(serverSocket);
+        WSACleanup();
+        return -1;
+    }
+
+    std::cout << "Čakám na hráčov...\n";
+
+    // Akceptovanie pripojenia
+    SOCKET clientSocket = accept(serverSocket, NULL, NULL);
+    if (clientSocket == INVALID_SOCKET) {
+        std::cerr << "Chyba pri akceptovaní pripojenia\n";
+        closesocket(serverSocket);
+        WSACleanup();
+        return -1;
+    }
+
+    std::cout << "Hráč pripojený!\n";
+
+    // Spravujte hru a komunikáciu s hráčmi
+
+    // Zatvorenie socketov
+    closesocket(clientSocket);
+    closesocket(serverSocket);
+    WSACleanup();
+
     return 0;
 }
