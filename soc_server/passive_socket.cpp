ef5ca559a6c4266c87b600d4001bdb46eee4c7bd
adams <adamstano5@gmail.com>
2024-01-03 13:12:15 +0100

    passive_socket

--- soc_server/passive_socket.cpp
+++ soc_server/passive_socket.cpp
@@ -1,5 +1,115 @@
-//
-// Created by adams on 3. 1. 2024.
-//
-
 #include "passive_socket.h"
+#include "active_socket.h"
+
+PassiveSocket::PassiveSocket() : socketDescriptor(INVALID_SOCKET), isListening(false), isWaiting(false) {
+    InitializeCriticalSection(&mutex);
+    InitializeConditionVariable(&waitingFinished);
+}
+
+PassiveSocket::~PassiveSocket() {
+    if (socketDescriptor != INVALID_SOCKET) {
+        closesocket(socketDescriptor);
+    }
+    socketDescriptor = INVALID_SOCKET;
+    isListening = false;
+    isWaiting = false;
+    DeleteCriticalSection(&mutex);
+}
+
+bool PassiveSocket::startListening(short port) {
+    EnterCriticalSection(&mutex);
+    if (isListening || isWaiting) {
+        LeaveCriticalSection(&mutex);
+        std::cerr << "PassiveSocket is listening. Cannot call startListening again." << std::endl;
+        return false;
+    }
+
+    socketDescriptor = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketDescriptor == INVALID_SOCKET) {
+        perror("socket failed.");
+        LeaveCriticalSection(&mutex);
+        return false;
+    }
+
+    sockaddr_in serverAddress{};
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(port);
+
+    if (bind(socketDescriptor, reinterpret_cast<sockaddr*>(&serverAddress), sizeof(serverAddress)) == SOCKET_ERROR) {
+        perror("bind failed.");
+        LeaveCriticalSection(&mutex);
+        return false;
+    }
+
+    listen(socketDescriptor, 10);
+    isListening = true;
+    LeaveCriticalSection(&mutex);
+
+    return true;
+}
+
+void PassiveSocket::stopListening() {
+    EnterCriticalSection(&mutex);
+    if (!isListening) {
+        LeaveCriticalSection(&mutex);
+        return;
+    }
+
+    isListening = false;
+    while (isWaiting) {
+        SleepConditionVariableCS(&waitingFinished, &mutex, INFINITE);
+    }
+
+    closesocket(socketDescriptor);
+    socketDescriptor = INVALID_SOCKET;
+
+    LeaveCriticalSection(&mutex);
+}
+
+bool PassiveSocket::isListeningNow() {
+    EnterCriticalSection(&mutex);
+    bool listening = isListening;
+    LeaveCriticalSection(&mutex);
+    return listening;
+}
+
+bool PassiveSocket::waitForClient(ActiveSocket* clientSock) {
+    EnterCriticalSection(&mutex);
+    if (isWaiting) {
+        LeaveCriticalSection(&mutex);
+        std::cerr << "PassiveSocket is waiting for a client. Cannot call waitForClient again." << std::endl;
+        return false;
+    }
+
+    isWaiting = true;
+    LeaveCriticalSection(&mutex);
+
+    sockaddr_in clientAddress{};
+    int clientAddressLength = sizeof(clientAddress);
+
+    while (isListeningNow()) {
+        SOCKET clientSocket = accept(socketDescriptor, reinterpret_cast<sockaddr*>(&clientAddress), &clientAddressLength);
+
+        if (clientSocket == INVALID_SOCKET) {
+            perror("accept failed.");
+            return false;
+        }
+
+        clientSock->setSocketDescriptor(clientSocket);
+
+        EnterCriticalSection(&mutex);
+        isWaiting = false;
+        LeaveCriticalSection(&mutex);
+        WakeConditionVariable(&waitingFinished);
+
+        return true;
+    }
+
+    EnterCriticalSection(&mutex);
+    isWaiting = false;
+    WakeConditionVariable(&waitingFinished);
+    LeaveCriticalSection(&mutex);
+
+    return false;
+}
--- soc_server/passive_socket.h
+++ soc_server/passive_socket.h
@@ -1,33 +1,31 @@
-//
-// Created by adams on 3. 1. 2024.
-//
+#ifndef PASSIVE_SOCKET_H
+#define PASSIVE_SOCKET_H
 
-#ifndef CLOVECENEZLOBSA_PASSIVE_SOCKET_H
-#define CLOVECENEZLOBSA_PASSIVE_SOCKET_H
-
+#include <iostream>
 #include <winsock2.h>
-#include "active_socket.h"
 
-struct passive_socket {
-    SOCKET socket_descriptor;
-    bool is_listening;
-    bool is_waiting;
-    CRITICAL_SECTION mutex;
-    CONDITION_VARIABLE waiting_finished;
+class ActiveSocket;
 
+class PassiveSocket {
+private:
+    SOCKET socketDescriptor;
+    bool isListening;
+    bool isWaiting;
+    CRITICAL_SECTION mutex{};
+    CONDITION_VARIABLE waitingFinished{};
+
+public:
+    PassiveSocket();
+
+    ~PassiveSocket();
+
+    bool startListening(short port);
+
+    void stopListening();
+
+    bool isListeningNow();
+
+    bool waitForClient(ActiveSocket* clientSock);
 };
 
-void passive_socket_init(struct passive_socket *self);
-
-void passive_socket_destroy(struct passive_socket *self);
-
-bool passive_socket_start_listening(struct passive_socket *self, short port);
-
-void passive_socket_stop_listening(struct passive_socket *self);
-
-bool passive_socket_is_listening(struct passive_socket *self);
-
-bool passive_socket_wait_for_client(struct passive_socket *self, struct active_socket *client_sock);
-
-
-#endif //CLOVECENEZLOBSA_PASSIVE_SOCKET_H
+#endif // PASSIVE_SOCKET_H
