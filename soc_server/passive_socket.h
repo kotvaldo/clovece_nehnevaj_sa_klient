10255512c8aba3a023b76d39c5ec6c7d51e5941c
adams <adamstano5@gmail.com>
2024-01-03 13:02:25 +0100

    passive_socket

--- soc_server/passive_socket.h
+++ soc_server/passive_socket.h
@@ -5,10 +5,29 @@
 #ifndef CLOVECENEZLOBSA_PASSIVE_SOCKET_H
 #define CLOVECENEZLOBSA_PASSIVE_SOCKET_H
 
+#include <winsock2.h>
+#include "active_socket.h"
 
-class passive_socket {
+struct passive_socket {
+    SOCKET socket_descriptor;
+    bool is_listening;
+    bool is_waiting;
+    CRITICAL_SECTION mutex;
+    CONDITION_VARIABLE waiting_finished;
 
 };
+
+void passive_socket_init(struct passive_socket *self);
+
+void passive_socket_destroy(struct passive_socket *self);
+
+bool passive_socket_start_listening(struct passive_socket *self, short port);
+
+void passive_socket_stop_listening(struct passive_socket *self);
+
+bool passive_socket_is_listening(struct passive_socket *self);
+
+bool passive_socket_wait_for_client(struct passive_socket *self, struct active_socket *client_sock);
 
 
 #endif //CLOVECENEZLOBSA_PASSIVE_SOCKET_H
