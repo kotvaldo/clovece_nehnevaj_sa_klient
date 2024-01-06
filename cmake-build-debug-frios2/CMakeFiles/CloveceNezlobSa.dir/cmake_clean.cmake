821f5d70d0c137a0f1cbb9dbe5a97e87e653b883
adams <adamstano5@gmail.com>
2024-01-06 18:41:54 +0100

    -Winsock trasform + makefile.bat

---
+++ cmake-build-debug-frios2/CMakeFiles/CloveceNezlobSa.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CloveceNezlobSa.dir/client.cpp.o"
+  "CMakeFiles/CloveceNezlobSa.dir/sockets/client_socket.cpp.o"
+  "CloveceNezlobSa"
+  "CloveceNezlobSa.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CloveceNezlobSa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug-mingw/CMakeFiles/CloveceNezlobSa.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CloveceNezlobSa.dir/client.cpp.obj"
+  "CMakeFiles/CloveceNezlobSa.dir/client.cpp.obj.d"
+  "CMakeFiles/CloveceNezlobSa.dir/sockets/client_socket.cpp.obj"
+  "CMakeFiles/CloveceNezlobSa.dir/sockets/client_socket.cpp.obj.d"
+  "CloveceNezlobSa.exe"
+  "CloveceNezlobSa.exe.manifest"
+  "CloveceNezlobSa.pdb"
+  "libCloveceNezlobSa.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CloveceNezlobSa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
